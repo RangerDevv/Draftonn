753f260008e1973268464002e46e9920485f0b53
avidcoder123 <umar.tli.edu@gmail.com>
2023-07-16 19:59:26 +0000

    Note button

--- src/components/global/createbuttons.svelte
+++ src/components/global/createbuttons.svelte
@@ -25,12 +25,34 @@
             }
         ).then(_ => window.location.href = `${path}/${folderName}`)
     }
+
+    function newNote() {
+        let noteName = ""
+        while(noteName == "") {
+            //Replace later with Modal!
+            noteName = prompt("(REPLACE PROMPT WITH MODAL LATER) Enter note title:")
+        }
+
+        appwriteDatabases.createDocument(
+            DB_ID,
+            COLLECTION.Notes,
+            ID.unique(),
+            {
+                Name: noteName,
+                Location: path,
+                AuthorUid,
+                AuthorName,
+                IsPublic: false,
+
+            }
+        ).then(res => window.location.href = `/doc/${res.$id}`)
+    }
 </script>
 <div class="flex flex-row gap-2">
     <button on:click={newFolder} class="btn text-gray-800 bg-gray-200 items-center hover:bg-gray-300">
         New Folder
     </button>
-    <button class="btn text-gray-800 bg-gray-200 items-center hover:bg-gray-300">
+    <button on:click={newNote} class="btn text-gray-800 bg-gray-200 items-center hover:bg-gray-300">
         New Note
     </button>
 </div>
