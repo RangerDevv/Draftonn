942e870133c49b679900b96b60377dad38e25a18
avidcoder123 <umar.tli.edu@gmail.com>
2023-11-29 20:37:21 +0000

    modal

--- src/components/selectclasses.svelte
+++ src/components/selectclasses.svelte
@@ -5,6 +5,8 @@
 
     export let user: string
 
+    let className = "", teacherName = "", acceleration = ""
+
     let classes: Models.Document[] = []
 
     let selectedClasses: string[] = []
@@ -46,7 +48,7 @@
 <div class="flex flex-col items-center 2xl:scale-125 pt-10 gap-4">
     <p class="text-3xl text-center font-bold">Select your classes</p>
     <button class="h-12 bg-blue-500 rounded-md w-44" on:click={() => {
-        const modal = document.getElementById('my_modal_1');
+        const modal = document.getElementById('newclassmodal');
         if (modal instanceof HTMLDialogElement && typeof modal.showModal === 'function') {
             modal.showModal();
         }
@@ -65,17 +67,20 @@
 <dialog id="newclassmodal" class="modal">
     <form method="dialog" class="modal-box bg-gray-200 flex flex-col gap-4">
       <h3 class="font-bold text-2xl text-gray-900 text-center">Create Class</h3>
-      <label for="name" class="text-gray-800">Name</label>
-      <input type="text" id="name" class="input input-bordered bg-gray-400" placeholder="Name" />
-      <label for="database" class="text-gray-800">Type Of Document</label>
-      <select id="database" class="select select-bordered bg-gray-400">
-          <option value="648bc7024074897c154d">Notebook</option>
-          <option value="64d5a380c46a1248b02c">Todo List</option>
-          <option value="64dab46d57d73632fba5">Drawing</option>
+      <label for="name" class="text-gray-800">Class Name</label>
+      <input type="text" id="name" class="input input-bordered bg-gray-400" placeholder="Class Name" bind:value={className} />
+      <label for="name" class="text-gray-800">Professor/Teacher Name</label>
+      <input type="text" id="name" class="input input-bordered bg-gray-400" placeholder="Professor/Teacher Name" bind:value={teacherName} />
+      <label for="database" class="text-gray-800">Class Acceleration</label>
+      <select id="database" class="select select-bordered bg-gray-400" bind:value={acceleration}>
+          <option value="">Select...</option>
+          <option value="On Level">On Level</option>
+          <option value="Honors">Honors</option>
+          <option value="Dual Credit">Dual Credit</option>
+          <option value="AP">AP</option>
+          <option value="IB-HL">IB-HL</option>
       </select>
-      <label for="folder" class="text-gray-800">Folder</label>
-      <!--  createDocument() -->
-      <button class="btn btn-primary" on:click={() => {
+      <button class="btn btn-primary" disabled={!teacherName || !acceleration || !className} on:click={() => {
           const modal = document.getElementById('newclassmodal');
           if (modal instanceof HTMLDialogElement && typeof modal.close === 'function') {
               modal.close();
