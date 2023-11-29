1ca3c67c79e83b3ce8287a9b734017b39100115a
avidcoder123 <umar.tli.edu@gmail.com>
2023-11-29 18:27:11 +0000

    Class selection TODO: Create new classes

--- src/components/selectclasses.svelte
+++ src/components/selectclasses.svelte
@@ -1,15 +1,57 @@
 <script lang="ts">
-    export let user: string
-    export let username: string
+    import { Query, ID, type Models } from "appwrite";
+    import { appwriteDatabases } from "../lib/backend";
+    import { COLLECTION, DB_ID } from "../lib/ids";
 
-    let classes: {id: string, name: string}[] = []
+    export let user: string
+
+    let classes: Models.Document[] = []
+
+    let selectedClasses: string[] = []
+
+    appwriteDatabases.listDocuments(DB_ID, COLLECTION.User_School, [
+        Query.equal("User", user)
+    ])
+    .then(res => res.documents[0])
+    .then(doc => {
+        let school = doc.School
+        return appwriteDatabases.listDocuments(DB_ID, COLLECTION.Classes, [
+            Query.equal("School", school)
+        ])
+    })
+    .then(res => {
+        console.log(res)
+        classes = res.documents
+    })
+
+    function updateClasses() {
+        appwriteDatabases.listDocuments(DB_ID, COLLECTION.Class_User, [
+            Query.equal("User", user)
+        ]).then(res => {
+            Promise.all(
+                res.documents.map(
+                    doc => appwriteDatabases.deleteDocument(DB_ID, COLLECTION.Class_User, doc.$id)
+                )
+            )
+        }).then(() => {
+            return Promise.all(selectedClasses.map(c => 
+                appwriteDatabases.createDocument(DB_ID, COLLECTION.Class_User, ID.unique(), {
+                    Class: c,
+                    User: user
+                })
+            ))
+        }).then(() => window.location.href = "/dashboard")
+    }
 </script>
-<div class="flex justify-center h-56 sm:pt-10 2xl:scale-125 pt-12">
-    <p class="text-3xl text-center h-44 font-bold pt-16">Select your classes</p>
-    {#each classes as c}
-        <label class="cursor-pointer label">
-            <input value={c.id} type="checkbox" bind:group={classes} class="checkbox checkbox-info checkbox-lg" />
-            <span class="label-text ml-2 text-lg text-white">{c.name}</span>
-        </label>
-    {/each}
+<div class="flex flex-col items-center 2xl:scale-125 pt-10 gap-4">
+    <p class="text-3xl text-center font-bold">Select your classes</p>
+    <div>
+        {#each classes as c}
+            <label class="cursor-pointer label">
+                <input value={c.$id} bind:group={selectedClasses} type="checkbox" class="checkbox checkbox-info checkbox-lg" />
+                <span class="label-text ml-2 text-lg">{c.Name}</span>
+            </label>
+        {/each}
+    </div>
+    <button class="bg-gray-400 rounded-md w-36 h-12" on:click={updateClasses} disabled={selectedClasses.length == 0}>Save</button>
 </div>
--- src/lib/ids.ts
+++ src/lib/ids.ts
@@ -5,5 +5,7 @@ export const COLLECTION = {
     Todo: "64d5a380c46a1248b02c",
     Drawing: "64dab46d57d73632fba5",
     Schools: "6564e25da3c12066d80e",
-    User_School: "6566256e1589df22e392"
+    User_School: "6566256e1589df22e392",
+    Classes: "65677095b9a64d794924",
+    Class_User: "656771844e22e5b07165"
 }
