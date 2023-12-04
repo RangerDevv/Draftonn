4290d056bf6c594634be929a5f16bfb2e6455277
avidcoder123 <umar.tli.edu@gmail.com>
2023-12-04 19:57:19 +0000

    Mashallah

--- src/components/global/createunit.svelte
+++ src/components/global/createunit.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
-    import { Query } from "appwrite";
+    import { ID, Query } from "appwrite";
     import { appwriteDatabases } from "../../lib/backend";
     import { COLLECTION, DB_ID } from "../../lib/ids";
 
     export let id: string;
 
     function create() {
+        let attempt = prompt("Enter the folder name:")
         appwriteDatabases.listDocuments(DB_ID, COLLECTION.Units, [
             Query.equal("Class", id)
         ]).then(x => x.documents)
         .then(units => {
-            
+            console.log("h")
+            if(!attempt || units.map(u => (u.Name as string).toLowerCase()).includes(attempt.toLowerCase())) {
+                return alert("Invalid Unit Name.")
+            } else {
+                return appwriteDatabases.createDocument(DB_ID, COLLECTION.Units, ID.unique(), {
+                    Name: attempt,
+                    Class: id
+                }).then(() => {
+                    location.reload()
+                })
+            }
         })
     }
 </script>
